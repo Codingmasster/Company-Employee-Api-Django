d528b0efcb7f7c786073cd78f1418ec1118b3468
Code Master <bughunter255@gmail.com>
2024-05-04 18:50:45 +0500

    Added Employee Model

--- api/models.py
+++ api/models.py
@@ -10,5 +10,11 @@ class Company(models.Model):
     added_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
-
-    
+class Employee(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    about = models.TextField()
+    type = models.CharField(max_length=100,choices=(('IT','IT'),('NON IT','NON IT'),('MOBILE Company','MOBILE Company')))
+    hire_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
