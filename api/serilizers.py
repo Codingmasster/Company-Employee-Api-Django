6e9e27213fc4870d023a86020b6b8b77df327433
Code Master <bughunter255@gmail.com>
2024-05-04 18:51:22 +0500

    Added CompanySerializer

--- api/serilizers.py
+++ api/serilizers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from api.models import Company
+from api.models import Company,Employee
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Company
+        fields = "__all__"
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=Employee
         fields = "__all__"
