d655550e7056ad59587122c24b74aaf2ed885435
Code Master <bughunter255@gmail.com>
2024-05-04 18:56:20 +0500

    Created EmployeViewSet

--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from api.models import Company
-from api.serilizers import CompanySerializer
+from api.models import Company,Employee
+from api.serilizers import CompanySerializer,EmployeeSerializer
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    
 # Create your views here.
